addcf1e75fff312d2fa1d449afe505dc4528336a
Ngyyyennnn <54529371+tqnguyen19it3@users.noreply.github.com>
2021-06-05 18:04:04 +0700

    first commit

---
+++ set_repetitive_exact_alarm-master/app/src/main/java/com/foodiedev/setrepetitiveexactalarm/service/AlarmService.kt
@@ -0,0 +1,77 @@
+package com.foodiedev.setrepetitiveexactalarm.service
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.Toast
+import com.foodiedev.setrepetitiveexactalarm.receiver.AlarmReceiver
+import com.foodiedev.setrepetitiveexactalarm.util.Constants
+import com.foodiedev.setrepetitiveexactalarm.util.RandomUtil
+import java.util.*
+
+class AlarmService(private val context: Context) {
+    private val alarmManager: AlarmManager? =
+        context.getSystemService(Context.ALARM_SERVICE) as AlarmManager?
+
+
+    fun setExactAlarm(timeInMillis: Long) {
+        setAlarm(
+            timeInMillis,
+            getPendingIntent(
+                getIntent().apply {
+                    action = Constants.ACTION_SET_EXACT
+                    putExtra(Constants.EXTRA_EXACT_ALARM_TIME, timeInMillis)
+                    Toast.makeText(context, "Thiết lập thông báo thành công", Toast.LENGTH_LONG).show();
+                }
+            )
+        )
+    }
+
+    //1 Week
+    fun setRepetitiveAlarm(timeInMillis: Long) {
+        setAlarm(
+            timeInMillis,
+            getPendingIntent(
+                getIntent().apply {
+                    action = Constants.ACTION_SET_REPETITIVE_EXACT
+                    putExtra(Constants.EXTRA_EXACT_ALARM_TIME, timeInMillis)
+                    Toast.makeText(context, "Thiết lập thông báo thành công", Toast.LENGTH_LONG).show();
+                }
+            )
+        )
+    }
+
+    private fun getPendingIntent(intent: Intent) =
+        PendingIntent.getBroadcast(
+            context,
+            getRandomRequestCode(),
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+
+    private fun setAlarm(timeInMillis: Long, pendingIntent: PendingIntent) {
+        alarmManager?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmManager.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    timeInMillis,
+                    pendingIntent
+                )
+            } else {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    timeInMillis,
+                    pendingIntent
+                )
+            }
+        }
+    }
+
+    private fun getIntent() = Intent(context, AlarmReceiver::class.java)
+
+    private fun getRandomRequestCode() = RandomUtil.getRandomInt()
+
+}
